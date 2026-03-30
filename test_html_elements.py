ed85685cfba5f8631d346268dc97309679310e70
TibaAlMafrachi <almafrtk@miamioh.edu>
2026-03-29 22:14:29 -0400

    Create test_html_elements.py

---
+++ test_html_elements.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+import unittest
+
+class TestH5Tag(unittest.TestCase):
+    def setUp(self):
+        # Setup Firefox options
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")  # Ensures the browser window does not open
+        firefox_options.add_argument("--no-sandbox")
+        firefox_options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Firefox(options=firefox_options)
+
+def test_h5_tag_content(self):
+    driver = self.driver
+    driver.get("http://jenkins-225.cit.regionals.miamioh.edu/almafrtk6/")
+
+    print("CURRENT URL:", driver.current_url)
+    print("PAGE TITLE:", driver.title)
+    print("PAGE SOURCE START:")
+    print(driver.page_source[:1000])
+
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+
+    wait = WebDriverWait(driver, 10)
+    h5_element = wait.until(EC.presence_of_element_located((By.TAG_NAME, "h5")))
+    h5_text = h5_element.text
+
+    self.assertEqual("Lab 3-7 Works!", h5_text, "The <h5> tag does not contain the text 'Lab 3-7 Works!'")
+
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
